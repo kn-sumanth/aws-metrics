6b1ededf384402e4d80c137ac3f67da4facf726c
kn-sumanth <@sumanth>
2024-03-18 12:56:23 +0100

    add context

--- aws_metrics/metrics.py
+++ aws_metrics/metrics.py
@@ -1,28 +1,47 @@
 import boto3
 from typing import Any
+import time
+
 _client = None
 
 # individual metrics
 _metrics = []
 
 # Namespace for all the metrics
-_namespace = 'Unknown'
+_namespace = 'unknown'
 
 # Dimenions common for all metrics
 _dimensions = {}
+
+# Context related to the metrics
+_context = {}
 
 def init_client():
     global _client
     _client = boto3.client('cloudwatch')
 
-
 def init_metrics(namespace: str):
     global _metrics
     global _namespace
     global _dimensions
+    global _context
     _metrics = list()
     _namespace = namespace
     _dimensions = dict()
+    _context = dict()
+    _context['_init_mono_time'] = time.monotonic()
+
+def set_common_dimension(key: str, value: str):
+    _dimensions[key] = value
+
+def set_common_dimensions(dimensions: dict):
+    _dimensions.update(dimensions)
+
+def set_context(key: str, value: Any):
+    _context[key] = value
+
+def get_context(key: str, default: Any = None):
+    return _context.get(key, default)
 
 def add_metric(name: str, value: Any, unit: str, dimensions: dict = None):
     _metrics.append({
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,10 @@
-from aws_metrics.metrics import init_client, init_metrics, add_metric
+from aws_metrics import metrics
 
 
 def test_flow():
-    init_client()
-    init_metrics('TestNamespace')
-    add_metric('TestMetric', 10, 'Count', {'TestDimension': 'TestValue'})
+    metrics.init_client()
+    metrics.init_metrics('TestNamespace')
+    metrics.set_common_dimension('TestDimension', 'TestValue')
+    metrics.set_context('TestContext', 'TestValue')
+    metrics.get_context('_init_mono_time')
+    metrics.add_metric('TestMetric', 10, 'Count', {'TestDimension': 'TestValue'})
